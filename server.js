f613bf307166ac2c110acef9f329a761e8102612
Muhammad Awan <ayzad01@gmail.com>
2018-11-29 22:30:12 -0500

    Added project page

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
-
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -40,6 +36,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
